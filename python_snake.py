4d59f5fe832271bf7ba015743f9f7c635983a31e
marcos-cmd <marcos.joseph.garcia@gmail.com>
2021-02-19 20:51:13 -0800

    added 'you lost' message

--- python_snake.py
+++ python_snake.py
@@ -6,18 +6,31 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
-dis = pygame.display.set_mode((800, 600))
+dis_width = 800
+dis_height = 600
+dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Me')
 
 game_over = False
 
-x1 = 300
-y1 = 300
+x1 = dis_width/2
+y1 = dis_height/2
+
+snake_block = 10
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+snake_speed = 30
+
+font_style = pygame.font.SysFont(None, 50)
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+
 
 while not game_over:
     for event in pygame.event.get():
@@ -37,14 +50,21 @@ while not game_over:
             elif event.key == pygame.K_DOWN:
                 x1_change = 0
                 y1_change = 10
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
+
     x1 += x1_change
     y1 += y1_change
     dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+    pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
 
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(snake_speed)
+
+message("You lost", red)
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()
 quit()
